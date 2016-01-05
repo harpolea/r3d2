045bcc4f0eac4bd62ec64b72fe9309ec2ed53426
Ian Hawke <I.Hawke@soton.ac.uk>
2016-01-05 12:53:13 +0000

    Remove multi gamma EOS.
    With the way this code is formulated, on a per-wave basis, it doesn't make sense.

--- eos_defns.py
+++ eos_defns.py
@@ -17,19 +17,6 @@ def eos_gamma_law(gamma):
 
     return eos
 
-def eos_multi_gamma_law(gamma, wave_i):
-
-    p_from_rho_eps = lambda rho, eps : (gamma[wave_i] - 1.0) * rho * eps
-    h_from_rho_eps = lambda rho, eps : 1.0 + gamma[wave_i] * eps
-    cs_from_rho_eps = lambda rho, eps : \
-    np.sqrt(gamma[wave_i] * p_from_rho_eps(rho, eps) / (rho * h_from_rho_eps(rho, eps)))
-
-    eos = {'p_from_rho_eps' : p_from_rho_eps,
-           'h_from_rho_eps' : h_from_rho_eps,
-           'cs_from_rho_eps' : cs_from_rho_eps}
-
-    return eos
-
 def eos_polytrope_law(gamma, gamma_th, rho_transition, k):
 
     def p_from_rho_eps(rho, eps):
