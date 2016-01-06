2b0fd9be18aeacadf395fab47937283798eef9ab
harpolea <ah1e14@soton.ac.uk>
2016-01-06 10:32:23 +0000

    Find_left works for arbitrary EoS

--- find_left_state.py
+++ find_left_state.py
@@ -7,19 +7,19 @@ import SR1d
 import numpy as np
 from scipy.optimize import brentq
 
-def find_left(q_r, M=1., gamma=5./3., K=1.):
+def find_left(q_r, eos, M=1.):
     """
-    Finds the left wave for a given Mach number and right state.
+    Finds the left wave for a given Mach number and initial right state.
 
-    Returns the left hand state.
+    It can take arbitrary equation of state.
+
+    Returns the left hand state and speed of the shock wave.
     """
 
-    # calculate sound speed
-    # for gamma law, h * c_s**2 = 2 gamma * p / rho
     rho_r = q_r.rho
-    p_r = K * rho_r**gamma
-    h = 1. + p_r * gamma / (rho_r * (gamma - 1.))
-    c_s = np.sqrt(2. * gamma * p_r / (rho_r * h))
+    eps_r = q_r.eps
+    p_r = eos['p_from_rho_eps'](rho_r, eps_r)
+    c_s = eos['cs_from_rho_eps'](rho_r, eps_r)
 
     print('c_s = {}'.format(c_s))
 
@@ -42,11 +42,22 @@ def find_left(q_r, M=1., gamma=5./3., K=1.):
 
         return M_star - M
 
-    p_star = brentq(find_p_star, p_star_lims[0], p_star_lims[1], args=(q_r, c_s))
+    try:
+        p_star = brentq(find_p_star, p_star_lims[0], p_star_lims[1], args=(q_r, c_s))
+    except ValueError:
+        # brentq will have failed due to the limits, so try
+        # lowering/raising them
+        if M < 1.:
+            p_star_lims[0] *= 1.e-5
+        else:
+            p_star_lims[1] *= 1.e4
+
+        p_star = brentq(find_p_star, p_star_lims[0], p_star_lims[1], args=(q_r, c_s))
 
     wave_r = SR1d.Wave(q_r, p_star, 2)
 
-    return wave_r.q_l
+    return wave_r.q_l, wave_r.wave_speed[0]
+
 
 if __name__ == "__main__":
 
@@ -56,11 +67,12 @@ if __name__ == "__main__":
     eos = eos_defns.eos_gamma_law(gamma)
 
     # right state
-    rho_r, v_r, vt_r = (0.1, 0.0, 0.0)
+    rho_r, v_r, vt_r = (0.001, 0.0, 0.0)
     eps_r = K * rho_r**(gamma - 1.) / (gamma - 1.)
 
     # initialise right state
     q_r = SR1d.State(rho_r, v_r, vt_r, eps_r, eos, label="R")
 
-    q_l = find_left(q_r, M=4., gamma=gamma, K=K)
+    q_l, v_s = find_left(q_r, eos, M=10.)
     print('left state [rho,  v,  vt,  eps] =  {}'.format(q_l.prim()))
+    print('wave speed = {}'.format(v_s))
