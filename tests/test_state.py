cad948dca6477466b807f38550a0318fe4c2b51c
Alice Harpole <alice.harpole@soton.ac.uk>
2018-07-06 11:32:06 +0100

    Fixed test

--- tests/test_state.py
+++ tests/test_state.py
@@ -7,7 +7,7 @@ def test_basic_state():
     """
     Simple gamma law, inert state
     """
-    
+
     eos = eos_defns.eos_gamma_law(5.0/3.0)
     U = State(1.0, 0.0, 0.0, 1.5, eos, label="Test")
     state = U.state()
@@ -16,7 +16,7 @@ def test_basic_state():
     string = r"\begin{pmatrix} \rho \\ v_x \\ v_t \\ \epsilon \end{pmatrix}_{Test}"
     string += r" = \begin{pmatrix} 1.0000 \\ 0.0000 \\ 0.0000 \\ 1.5000 \end{pmatrix}"
     assert U.latex_string() == string
-    assert U._repr_latex_() == r"$" + string + r"$"
+    assert U._repr_latex_() == r"\begin{equation}" + string + r"\end{equation}"
     assert_allclose(U.wavespeed(0), -sqrt(10.0/21.0))
     assert_allclose(U.wavespeed(1), 0.0)
     assert_allclose(U.wavespeed(2), +sqrt(10.0/21.0))
@@ -26,7 +26,7 @@ def test_reactive_state():
     """
     Reactive EOS.
     """
-    
+
     eos = eos_defns.eos_gamma_law(5.0/3.0)
     eos_reactive = eos_defns.eos_gamma_law_react(5.0/3.0, 1.0, 1.0, 1.0, eos)
     U = State(1.0, 0.0, 0.0, 1.5, eos_reactive, label="Test")
